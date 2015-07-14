710f87a18a19834863864a0c1242cef4323ad94e
tomgraion <tomas.ferraro@graion.com>
2015-07-14 00:38:08 -0300

    Proper URL fix

--- js/services/mainServices.js
+++ js/services/mainServices.js
@@ -1,15 +1,19 @@
 'use strict';
 
-baseServices .factory('Icons', ['$http', '$q', '$filter', '$timeout','Dificultad',
-  function($http, $q, $filter, $timeout , Dificultad){
+baseServices .factory('Icons', ['$http', '$q', '$filter', '$timeout','Dificultad','ProperURL',
+  function($http, $q, $filter, $timeout , Dificultad, ProperURL ){
 
     function getAll(){
 
-        var defer = $q.defer();
+        var defer = $q.defer(),
+          url = ProperURL.returnURL('data/icons.json');
 
-        $http.get('/data/icons.json')
+        $http.get(url)
             .success(function(data) {
-                defer.resolve(data);
+              defer.resolve(data);
+            })
+            .error(function(){
+              alert(data);
             });
 
         return defer.promise;
@@ -19,9 +23,10 @@ baseServices .factory('Icons', ['$http', '$q', '$filter', '$timeout','Dificultad
     function getByCategoryDificulty(category){
 
         var defer = $q.defer(),
-            lengthItems = Dificultad.getDificultad().items;
+            lengthItems = Dificultad.getDificultad().items,
+             url = ProperURL.returnURL('data/icons.json');
 
-        $http.get('/data/icons.json')
+        $http.get(url)
             .success(function(data) {
                 var icons = data.filter(function(entry){
                     return entry.id_categoria == category;
@@ -33,47 +38,28 @@ baseServices .factory('Icons', ['$http', '$q', '$filter', '$timeout','Dificultad
         return defer.promise;
     }
 
-    function get(iconId) {
-        var defer = $q.defer();
-
-        $http.get('/advertiser/' + iconId)
-            .success(function(data) {
-                defer.resolve(data);
-            })
-
-        return defer.promise;
-    }
-
-    function save(data) {
-        var defer = $q.defer();
-
-        $http.post('/advertiser', data)
-            .success(function(response) {
-                defer.resolve(response);
-            })
-
-        return defer.promise;
-    }
 
 
     return {
         getAll: getAll,
-        get: get,
-        save: save,
         getByCategoryDificulty : getByCategoryDificulty
         }
 
   }]);
 
-baseServices .factory('Categorias', ['$http', '$q', '$filter', '$timeout',
-  function($http, $q, $filter, $timeout){
+baseServices .factory('Categorias', ['$http', '$q', '$filter', '$timeout','ProperURL',
+  function($http, $q, $filter, $timeout, ProperURL){
 
     function getAll(){
-        var defer = $q.defer();
+        var defer = $q.defer(),
+         url = ProperURL.returnURL('data/categorias.json');
 
-        $http.get('/data/categorias.json')
+        $http.get(url)
             .success(function(advertisers) {
                 defer.resolve(advertisers);
+            })
+            .error(function(){
+              alert('erro');
             });
         return defer.promise;
     }
@@ -84,13 +70,15 @@ baseServices .factory('Categorias', ['$http', '$q', '$filter', '$timeout',
 
 }]);
 
-baseServices .factory('Dificultad', ['$http', '$q', '$filter', '$timeout',
-  function($http, $q, $filter, $timeout){
+baseServices .factory('Dificultad', ['$http', '$q', '$filter', '$timeout','ProperURL',
+  function($http, $q, $filter, $timeout, ProperURL){
     var selectedDificultad = {};
 
     function getAll(){
-        var defer = $q.defer();
-        $http.get('/data/dificultad.json')
+        var defer = $q.defer(),
+          url = ProperURL.returnURL('data/dificultad.json');
+
+        $http.get(url)
             .success(function(dificultad) {
                 defer.resolve(dificultad);
             });
@@ -115,92 +103,122 @@ baseServices .factory('Dificultad', ['$http', '$q', '$filter', '$timeout',
 
 }]);
 
+baseServices .factory('ProperURL', [
+  function(){
+
+    function returnURL (url){
+      if(ionic.Platform.isAndroid()){
+        url = "/android_asset/www/" + url;
+      }
+      return url;
+    }
+
+    return {
+        returnURL : returnURL
+      }
+
+}]);
+
 
 baseServices.factory('Media',['mediaLoader','shuffleArray',
     function(mediaLoader,shuffleArray){
         var sounds = {},
-            current_sound = 0,
-            correct_sound = "sounds/correct.mp3",
-            wrong_sound = "sounds/wrong.mp3";
+          current_sound = 0,
+          correct_sound = "sounds/correct.mp3",
+          wrong_sound = "sounds/wrong.mp3";
 
 
         function setMedias(data){
-            var shuffled = shuffleArray.shuffle(data);
-              sounds = shuffled;
-              resetMedias();
+          var shuffled = shuffleArray.shuffle(data);
+            sounds = shuffled;
+            resetMedias();
         }
 
         function resetMedias (){
-            current_sound = 0;
+          current_sound = 0;
         }
 
         function play(){
-            var file = sounds[current_sound].url;
-                mediaLoader.load(file);
+          var file = sounds[current_sound].url;
+            mediaLoader.load(file);
         }
 
         function checkCorrect(id){
-            if(sounds[current_sound].id_par == id){
-                if(current_sound < sounds.length){
-                    current_sound ++;
-                }
-                mediaLoader.load(correct_sound);
-                return true;
-            }else{
-                mediaLoader.load(wrong_sound);
-                return false;
+          if(sounds[current_sound].id_par == id){
+            if(current_sound < sounds.length){
+                current_sound ++;
             }
+            mediaLoader.load(correct_sound);
+            return true;
+          }else{
+            mediaLoader.load(wrong_sound);
+            return false;
+          }
         }
 
         return {
-            setMedias:setMedias,
-            play : play,
-            checkCorrect: checkCorrect
-            }
+          setMedias:setMedias,
+          play : play,
+          checkCorrect: checkCorrect
+        }
     }
 ]);
 
 baseServices.factory('mediaLoader', ['$q',
     function($q){
+      var mediaRes;
 
-        //ARREGLAR PARA TABLET Y QUE FRENE SONIDO
-        //var Media = new Media();
+        function media(src){
+          if (status === mediaRes.MEDIA_STOPPED) {
+             mediaRes = new Media(src,
+              function onSuccess() {
+                 // release the media resource once finished playing
+                 mediaRes.release();
+              },
+              function onError(e){
+                  console.log("error playing sound: " + JSON.stringify(e));
+              });
+           }
+        }
+
         function load(src){
-            var src;
-            if(ionic.Platform.isAndroid()){
-                src = '/android_asset/www/' + src;
+          var src;
+          if(ionic.Platform.isAndroid()){
+            src = '/android_asset/www/' + src;
+            media(src);
+            mediaRes.play();
 
-            }else{
-                new Audio(src).play();
-            }
+          }else{
+            new Audio(src).play();
+          }
         }
 
         return {
-            load:load
-            };
+          load:load
+        };
 }]);
 
 baseServices.factory('shuffleArray', [
 
     function(){
 
-        function shuffle(array) {
-          var m = array.length, t, i;
-          // While there remain elements to shuffle
-          while (m) {
-            // Pick a remaining element…
-            i = Math.floor(Math.random() * m--);
+      function shuffle(array) {
+        var m = array.length, t, i;
+        // While there remain elements to shuffle
+        while (m) {
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
 
-            // And swap it with the current element.
-            t = array[m];
-            array[m] = array[i];
-            array[i] = t;
-          }
-
-          return array;
+          // And swap it with the current element.
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
         }
 
-        return {
-            shuffle:shuffle
-            };
+        return array;
+      }
+
+      return {
+          shuffle:shuffle
+      };
 }])
